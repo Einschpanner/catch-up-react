b2860f48cdbf7abf0a3d64b8b8ba22c510ef8c0f
wwlee94 <wwlee94@naver.com>
2020-12-21 23:18:40 +0900

    Add : React App

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import { Button } from '@material-ui/core';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
